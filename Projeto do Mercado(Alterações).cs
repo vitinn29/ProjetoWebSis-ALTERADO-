6676e22347a44025de74d27f96026835037b50cb
vitinn29 <71273226+vitinn29@users.noreply.github.com>
2023-12-13 16:00:55 -0300

    Add files via upload

---
+++ Projeto do Mercado(Alterações).cs
@@ -0,0 +1,13 @@
+﻿using Mercadinho_do_Zé.View;
+
+namespace MercadinhoDoZe
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MenuView menu = new MenuView();
+            menu.CriarMenuPrincipal();
+        }
+    }
+}
